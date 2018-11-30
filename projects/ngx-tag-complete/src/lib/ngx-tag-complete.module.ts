1fd32b275966fb3681426d7c4f503252b5692977
josefan <jose.furtado@ajaxti.com.br>
2018-11-30 14:32:54 -0300

    Initial Commit

---
+++ projects/ngx-tag-complete/src/lib/ngx-tag-complete.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TagcompleteDirective } from './tagcomplete.directive';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [TagcompleteDirective],
+  exports: []
+})
+export class NgxTagCompleteModule { }
---
+++ projects/ngx-tag-complete/src/public_api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-tag-complete
+ */
+
+export * from './lib/tagcomplete.directive';
+export * from './lib/ngx-tag-complete.module';
